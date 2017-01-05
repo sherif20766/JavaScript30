04349e09c4c3ea935330bb979bf8eb854d2242d7
sherif20766 <sherif20766@hotmail.com>
2017-01-05 22:03:05 +0000

    completed project

---
+++ Flex_Gallery/main.js
@@ -0,0 +1,21 @@
+
+const panels = document.querySelectorAll(".panel");
+
+
+function toggleOpen(){
+	console.log("hello")
+	this.classList.toggle("open");
+} 
+
+
+function toggleActive(e) {
+
+	if(e.propertyName === "flex-grow"){
+		this.classList.toggle("open-active");
+	}
+
+}
+
+
+panels.forEach(panel => panel.addEventListener("click", toggleOpen));
+panels.forEach(panel => panel.addEventListener("transitionend", toggleActive));
